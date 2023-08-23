30b73964b90c1321361f7cb8a2184a7d6cea9b25
karl-jwlee <jwlee@lunasoft.co.kr>
2023-08-23 13:07:42 +0900

    card onclick event 수정

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 
 const Card = () => {
   const { pokemonList, setTargetPokemon } = usePokemonStore();
-  const setData = (items) => {
-    setTargetPokemon(items);
-  };
+  // const setData = (items) => {
+  //   setTargetPokemon(items);
+  // };
   return (
     <>
       {pokemonList.map((items, index) => {
         const url = "/detail?id=" + items.id;
         return (
           <div className="card-container-div" key={index}>
-            <Link to={url} onClick={() => setData(items)}>
+            <Link to={url} onClick={() => setTargetPokemon(items)}>
               <div className="img-area">
                 <img src={items.sprites.front_default} alt="pokemon" />
                 <div className="title">{items.ko_name}</div>
